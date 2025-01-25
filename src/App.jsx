8e087d53deb9cf231b68a26c9ab9ac7440b64fa8
Muhammad Abdullah Feroz <abdullahferoz313@gmail.com>
2025-01-25 20:42:45 +0500

    Added unctionality to change tabs using sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,24 @@ import Login from './components/Login'
 function App() {
   const [userName, setUserName] = useState("Guest User")
   const [authModal, setAuthModal] = useState(true)
+  const [tab, setTab] = useState("chat") 
 
-  const getData = (username, authenticated) => {
+  const getUserData = (username, authenticated) => {
     setUserName(username)
     setAuthModal(!authenticated)
   }
 
+  const getTabData = (tab)=>{
+    setTab(tab)
+    console.log(tab)
+  }
+
   return (
     <>{
-      authModal ? <Login onAuthentication={getData} /> :
+      authModal ? <Login onAuthentication={getUserData} /> :
         <div className='App h-screen w-screen flex flex-row'>
-          <Sidebar user="Abdullah Feroz" />
-          <Chat />
+          <Sidebar user="Abdullah Feroz" onTabChange = {getTabData} />
+          {tab == "chat" && <Chat />}
         </div>
     }
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
-const Sidebar = ({ user }) => {
+const Sidebar = (props) => {
 
   const [initials, setInitials] = useState("")
+  const [tab, setTab] = useState("chat")
 
   useEffect(() => {
-    if (user) {
-      const names = user.split(" ")
+    if (props.user) {
+      const names = props.user.split(" ")
       const initials = names[0]?.charAt(0) + names[1]?.charAt(0)
       setInitials(initials.toUpperCase()) // Make initials uppercase for consistency
     }
-  }, [user])
+  }, [props.user])
+
+  useEffect(() => {  
+   props.onTabChange(tab)
+  }, [tab])
+  
 
   return (
     <div className="sidebar w-[280px] h-full bg-blue-300 p-6 space-y-8">
@@ -24,19 +30,19 @@ const Sidebar = ({ user }) => {
       {/* Sidebar Menu */}
       <div className="menu cursor-pointer space-y-4">
         <div>
-          <div className="profile border-b-blue-800  p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
+          <div onClick={()=>{setTab("chat")}} className="chat border-b-blue-800  p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
             Chat
           </div>
           <div className='h-[1px] bg-blue-600' ></div>
         </div>
         <div>
-          <div className="profile  border-b-blue-800 p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
+          <div onClick={()=>{setTab("profile")}} className="profile  border-b-blue-800 p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
             Profile
           </div>
           <div className='h-[0.5px] bg-blue-600' ></div>
         </div>
         <div>
-          <div className="profile  border-b-blue-800 p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
+          <div onClick={()=>{setTab("logout")}} className="logout  border-b-blue-800 p-3 px-6 rounded-lg text-lg font-semibold text-white hover:bg-blue-500 hover:scale-105 transition-all">
             Log out
           </div>
           <div className='h-[1px] bg-blue-600' ></div>
