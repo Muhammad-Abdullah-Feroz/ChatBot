eb12b3cf6e8e407de23f2f6115ac553eb104d652
Muhammad Abdullah Feroz <abdullahferoz313@gmail.com>
2025-01-25 19:52:24 +0500

    Added functionality to authenticate the user

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import Login from './components/Login'
 
 function App() {
   const [userName, setUserName] = useState("Guest User")
-  const [count, setCount] = useState(0)
+  const [authModal, setAuthModal] = useState(true)
 
-  const getData = (username , authenticated) => {
-    console.log(username , authenticated)
+  const getData = (username, authenticated) => {
+    setUserName(username)
+    setAuthModal(!authenticated)
   }
 
   return (
-    <>
-    <Login onAuthentication = {getData}/>
-      {/* <div className='App h-screen w-screen flex flex-row'>
-        <Sidebar user = "Abdullah Feroz"/>
-        <Chat/>
-      </div> */}
+    <>{
+      authModal ? <Login onAuthentication={getData} /> :
+        <div className='App h-screen w-screen flex flex-row'>
+          <Sidebar user="Abdullah Feroz" />
+          <Chat />
+        </div>
+    }
     </>
   )
 }
